034a0309ce42d7e06a6f72ab331141341a7e96c4
alpha123131412413 <103320361+alpha123131412413@users.noreply.github.com>
2023-11-30 03:39:15 +0900

    메인 화면 업데이트

--- goat-frontend/src/pages/Home.jsx
+++ goat-frontend/src/pages/Home.jsx
@@ -7,16 +7,17 @@ function Home() {
   const goToLogIn = () => {
     navigate("/LogIn");
   };
+  
 
   return (
-    <div>
+    <div class="bg">
       <h1>Goat</h1>
       <button className="cta" onClick={goToLogIn}>
         <span>로그인</span>
-        <svg viewBox="0 0 13 10" height="10px" width="15px">
-          <path d="M1,5 L11,5"></path>
-          <polyline points="8 1 12 5 8 9"></polyline>
-        </svg>
+          <svg svg viewBox="0 0 13 10" height="10px" width="15px">
+            <path d="M1,5 L11,5"></path>
+            <polyline polyline points="8 1 12 5 8 9"></polyline>
+          </svg>
       </button>
     </div>
   );
