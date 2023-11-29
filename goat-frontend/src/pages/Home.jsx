d20078d1909e373c245578cc31b76463d9b4588d
leetaesik <vlwkvkd@naver.com>
2023-11-30 04:58:31 +0900

    프로토타입 최종

--- goat-frontend/src/pages/Home.jsx
+++ goat-frontend/src/pages/Home.jsx
@@ -9,13 +9,46 @@ function Home() {
   };
 
   return (
-    <div>
+    <div class="bg">
       <h1>Goat</h1>
-      <button className="cta" onClick={goToLogIn}>
-        <span>로그인</span>
-        <svg viewBox="0 0 13 10" height="10px" width="15px">
-          <path d="M1,5 L11,5"></path>
-          <polyline points="8 1 12 5 8 9"></polyline>
+      <h1 className="text">당신의 스포츠 매칭을 위한 완벽한 애플리케이션</h1>
+      <h3 className="text">
+        실시간 다양한 스포츠 종목 선택과 실력별 매칭으로 공정한 경기를 빠르고
+        쉽게 즐기세요!
+      </h3>
+      <button className="btn" onClick={goToLogIn}>
+        <div>로그인</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25px"
+          height="25px"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M11.6801 14.62L14.2401 12.06L11.6801 9.5"
+            stroke="black"
+            stroke-width="2"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            d="M4 12.0601H14.17"
+            stroke="black"
+            stroke-width="2"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            d="M12 4C16.42 4 20 7 20 12C20 17 16.42 20 12 20"
+            stroke="black"
+            stroke-width="2"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
         </svg>
       </button>
     </div>
--- goat-frontend/src/pages/LogIn.jsx
+++ goat-frontend/src/pages/LogIn.jsx
@@ -8,41 +8,43 @@ function LogIn() {
   const [pw, setPw] = useState("");
   const navigate = useNavigate();
   return (
-    <div className="box">
-      <h1>로그인 페이지</h1>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
+    <div className="bg">
+      <div className="box">
+        <h1>로그인 페이지</h1>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
 
-          console.log(id, pw);
-          navigate("/Main");
-        }}
-      >
-        <input
-          onChange={(event) => {
-            setId(event.target.value);
+            console.log(id, pw);
+            navigate("/Main");
           }}
-          required
-          type="text"
-          placeholder="아이디"
-          value={id}
-        />
+        >
+          <input
+            onChange={(event) => {
+              setId(event.target.value);
+            }}
+            required
+            type="text"
+            placeholder="아이디"
+            value={id}
+          />
 
-        <input
-          onChange={(event) => {
-            setPw(event.target.value);
-          }}
-          required
-          type="password"
-          placeholder="비밀번호"
-          value={pw}
-        />
+          <input
+            onChange={(event) => {
+              setPw(event.target.value);
+            }}
+            required
+            type="password"
+            placeholder="비밀번호"
+            value={pw}
+          />
 
-        <button>로그인</button>
-      </form>
-      <button className="signUp" onClick={() => navigate("/SignUp")}>
-        회원가입
-      </button>
+          <button>로그인</button>
+        </form>
+        <button className="signUp" onClick={() => navigate("/SignUp")}>
+          회원가입
+        </button>
+      </div>
     </div>
   );
 }
