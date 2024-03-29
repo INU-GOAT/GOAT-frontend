8405d79a3cb74e9d9d3f1e362f302bc4ba641b27
imKEISS <kss5122@inu.ac.kr>
2024-03-29 14:38:33 +0900

    로그인 페이지 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Main from "./pages/Main";
@@ -16,7 +15,6 @@ function App() {
           <Route path="/Main" element={<Main />} />
           <Route path="/Chat" element={<Chat />} />
         </Route>
-        <Route path="/LogIn" element={<LogIn />} />
         <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
