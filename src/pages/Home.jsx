ebbc665bc9892ef063a3bc0107ee15d20fcacd31
leetaesik <vlwkvkd@naver.com>
2024-04-01 03:47:26 +0900

    카카오로그인sdk 테스트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,15 @@
 import { useNavigate } from "react-router-dom";
 
 import "./css/Home.css";
+import KakaoLogIn from "./button/KakaoLogIn";
 
 function Home() {
   const navigate = useNavigate();
-  const goToLogIn = () => {
-    navigate("/LogIn");
-  };
-  
 
   return (
     <div class="bg">
       <h1>Goat</h1>
-      <button className="cta" onClick={goToLogIn}>
-        <span>로그인</span>
-          <svg svg viewBox="0 0 13 10" height="10px" width="15px">
-            <path d="M1,5 L11,5"></path>
-            <polyline polyline points="8 1 12 5 8 9"></polyline>
-          </svg>
-      </button>
+      <KakaoLogIn></KakaoLogIn>
     </div>
   );
 }
--- src/pages/button/KakaoLogIn.jsx
+++ src/pages/button/KakaoLogIn.jsx
@@ -1,50 +1,24 @@
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import KakaoLogin from "react-kakao-login";
 
-import "./css/LogIn.css";
-
-function LogIn() {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
-  const navigate = useNavigate();
+function KakaoLogIn() {
+  const kakaoClientId = "37417980d0926f07576f94eecf89bebf";
+  const kakaoOnSuccess = async (data) => {
+    console.log(data);
+    const idToken = data.response.access_token; // 엑세스 토큰 백엔드로 전달
+    console.log(idToken);
+  };
+  const kakaoOnFailure = (error) => {
+    console.log(error);
+  };
   return (
-    <div className="box">
-      <h1>로그인 페이지</h1>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-
-          console.log(id, pw);
-          navigate("/Main");
-        }}
-      >
-        <input
-          onChange={(event) => {
-            setId(event.target.value);
-          }}
-          required
-          type="text"
-          placeholder="아이디"
-          value={id}
-        />
-
-        <input
-          onChange={(event) => {
-            setPw(event.target.value);
-          }}
-          required
-          type="password"
-          placeholder="비밀번호"
-          value={pw}
-        />
-
-        <button>로그인</button>
-      </form>
-      <button className="signUp" onClick={() => navigate("/SignUp")}>
-        회원가입
-      </button>
-    </div>
+    <>
+      <KakaoLogin
+        token={kakaoClientId}
+        onSuccess={kakaoOnSuccess}
+        onFail={kakaoOnFailure}
+      />
+    </>
   );
 }
 
-export default LogIn;
+export default KakaoLogIn;
