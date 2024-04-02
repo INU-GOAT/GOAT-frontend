7467f83defd061df5c03016e2cc264335220e4f4
leetaesik <vlwkvkd@naver.com>
2024-04-03 06:44:00 +0900

    회원가입,탈퇴,로그아웃 api확인 전

--- src/pages/LogInHandler.jsx
+++ src/pages/LogInHandler.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { SyncLoader } from "react-spinners";
 import axios from "axios";
 
@@ -7,21 +7,44 @@ function LogInHandler() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const code = searchParams.get("code");
+  const [accessToken, setAccessToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
+
   console.log(code);
 
   useEffect(() => {
-    const LogIn = async () => {
+    const getToken = async () => {
       await axios
         .post("http://15.165.113.9:8080/api/users/code", { code }, {})
         .then((res) => {
           console.log(res);
+          setAccessToken(res.data.accessToken);
+          setRefreshToken(res.data.refreshToken);
         })
         .catch((error) => {
-          console.error("실패");
+          console.error("getToken 실패");
         });
     };
-    LogIn();
-  }, [code]);
+    const getUserId = async () => {
+      await axios
+        .get(`http://15.165.113.9:8080/api/users?Auth=${accessToken}`)
+        .then((res) => {
+          console.log(res);
+          if (res.data === -1) {
+            alert("회원가입이 필요합니다.");
+            navigate("/SignUp");
+          } else {
+            //id 값 이용 코드 추가 작성하기
+            navigate("/Main");
+          }
+        })
+        .catch((error) => {
+          console.error("getId 실패");
+        });
+    };
+    getToken();
+    getUserId();
+  }, [accessToken, code, navigate]);
 
   return (
     <div backgroundcolor="#9376E0">
