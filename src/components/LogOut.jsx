d9673efbe83e78618e28f11cd9b70d2495c9ea3a
leetaesik <vlwkvkd@naver.com>
2024-04-04 00:42:53 +0900

    navi

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 const Kakao = window;
 
 function LogOut() {
-  Navigate = useNavigate();
+  const navigate = useNavigate();
 
   const kakaoLogOut = () => {
     Kakao.Auth.logout()
       .then(function (response) {
         console.log(Kakao.Auth.getAccessToken()); // null
         //쿠키 지우기
-        Navigate("/");
+        navigate("/");
       })
       .catch(function (error) {
         console.log("Not logged in.");
