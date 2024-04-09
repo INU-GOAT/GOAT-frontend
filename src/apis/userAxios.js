ad9f77235aeb1e32ee4261a1b5aeb0e42f79da34
leetaesik <vlwkvkd@naver.com>
2024-04-10 03:22:37 +0900

    인터셉터헤더수정

--- src/apis/userAxios.js
+++ src/apis/userAxios.js
@@ -12,7 +12,7 @@ userAxios.interceptors.request.use(
     //요청 보내기 전에 수행 로직
     const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
-      config.headers.common["Auth"] = accessToken;
+      config.headers.Auth = accessToken;
     }
     return config;
   },
