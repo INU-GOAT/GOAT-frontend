818cefcd08a7c5662b08b6238d9a902fcf5b5e4f
leetaesik <vlwkvkd@naver.com>
2024-04-17 22:01:25 +0900

    sign out async await

--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -8,22 +8,21 @@ function SignOut() {
   const navigate = useNavigate();
   const { setIsLogin } = isLoginStore();
   const deleteUser = async () => {
-    await userAxios
-      .delete()
-      .then((res) => {
-        alert("회원탈퇴완료");
-        localStorage.clear();
-        setIsLogin(false);
-        navigate("/");
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("회원탈퇴실패");
-      });
+    try {
+      const res = await userAxios.delete();
+      console.log(res);
+      alert("회원탈퇴완료");
+      localStorage.clear();
+      setIsLogin(false);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      alert("회원탈퇴실패");
+    }
   };
 
-  const signOuthandler = async () => {
-    await deleteUser();
+  const signOuthandler = () => {
+    deleteUser();
   };
   return (
     <Button color="error" variant="contained" onClick={signOuthandler}>
