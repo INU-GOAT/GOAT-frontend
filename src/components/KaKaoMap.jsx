3a048be272de0bb89b7ebe7c3bd800b716f4459f
KJW <junewoon98@gmail.com>
2024-05-01 23:47:39 +0900

    kakomap보임,클럽수정

--- src/components/KaKaoMap.jsx
+++ src/components/KaKaoMap.jsx
@@ -12,6 +12,11 @@ export default function KakaoMap() {
         lat: 33.450701,
         lng: 126.570667,
       }}
+	  style={{
+        // 지도의 크기
+        width: "100%",
+        height: "450px",
+      }}
       level={3} // 지도의 확대 레벨
 	  
     />
--- src/pages/Club.jsx
+++ src/pages/Club.jsx
@@ -1,68 +1,77 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react'
+import { useState } from 'react';
+import "./css/Club.css";
 
 function Club() {
     const [clubname, setClubname] = useState('');
     const [intro, setIntro] = useState('');
     const [sport, setSport] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(`submit! ${clubname} ${setIntro}`);
-    };
-  
-    return (
-      <div className="Club">
-        <h1>클럽 제작</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            name="clubname"
-            value={clubname}
-            placeholder="클럽명"
-            onChange={(e) => setClubname(e.target.value)}
-          />
-          <input
-            name="intro"
-            value={intro}
-            placeholder="소개문"
-            onChange={(e) => setIntro(e.target.value)}
-          />
-          <input
-            type="radio"
-            name="soccer"
-            value={sport}
-            id="soccer"
-            onChange={(e) => setIntro(e.target.value)}
-            />
-           <label htmlFor="축구">Large</label>
-           <input
-            type="radio"
-            name="pingpong"
-            value={sport}
-            id="pingpong"
-            onChange={(e) => setIntro(e.target.value)}
-            />
-           <label htmlFor="탁구">Large</label>
-           <input
-            type="radio"
-            name="basketball"
-            value={sport}
-            id="basketball"
-            onChange={(e) => setIntro(e.target.value)}
-            />
-           <label htmlFor="농구">Large</label>
-           <input
-            type="radio"
-            name="sport"
-            value={sport}
-            id="badminton"
-            onChange={(e) => setIntro(e.target.value)}
-            />
-           <label htmlFor="배드민턴">Large</label>
 
-          <input type="submit" />
-        </form>
-      </div>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`submit! ${clubname} ${intro} ${sport}`);
+    };
+
+    return (
+        <div className="Club">
+            <h1>클럽 제작</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="clubname"
+                    value={clubname}
+                    placeholder="클럽명"
+                    onChange={(e) => setClubname(e.target.value)}
+                />
+                <input
+                    name="intro"
+                    value={intro}
+                    placeholder="소개문"
+                    onChange={(e) => setIntro(e.target.value)}
+                />
+                <label>
+                    <input
+                        type="radio"
+                        name="sport"
+                        value="soccer"
+                        checked={sport === "soccer"}
+                        onChange={(e) => setSport(e.target.value)}
+                    />
+                    축구
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="sport"
+                        value="pingpong"
+                        checked={sport === "pingpong"}
+                        onChange={(e) => setSport(e.target.value)}
+                    />
+                    탁구
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="sport"
+                        value="basketball"
+                        checked={sport === "basketball"}
+                        onChange={(e) => setSport(e.target.value)}
+                    />
+                    농구
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="sport"
+                        value="badminton"
+                        checked={sport === "badminton"}
+                        onChange={(e) => setSport(e.target.value)}
+                    />
+                    배드민턴
+                </label>
+                <input type="submit" />
+            </form>
+        </div>
     );
-  }
-  export default Club;
+}
+
+export default Club;
