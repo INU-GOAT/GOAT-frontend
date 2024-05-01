5ae356eee8730cd1f07f97f433c225f8b2d90c31
bs8841 <whqortjs@gmail.com>
2024-05-01 17:30:23 +0900

    5.1-1

--- src/components/Sport.jsx
+++ src/components/Sport.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { RadioTileGroup, RadioTile } from '@itwin/itwinui-react';
 
-const Sport = ({ sport, isSelected, onClick }) => (
-  <button className={isSelected ? 'selected' : ''} onClick={() => onClick(sport)}>
-    {sport}
-  </button>
+const Sport = ({ selectedSport, onClick }) => (
+  <RadioTileGroup className='demo-radio-tile-group'>
+    <RadioTile
+      label='축구'
+      checked={selectedSport === '축구'}
+      onChange={() => onClick('축구')}
+    />
+    <RadioTile
+      label='농구'
+      checked={selectedSport === '농구'}
+      onChange={() => onClick('농구')}
+    />
+    <RadioTile
+      label='배드민턴'
+      checked={selectedSport === '배드민턴'}
+      onChange={() => onClick('배드민턴')}
+    />
+    <RadioTile
+      label='탁구'
+      checked={selectedSport === '탁구'}
+      onChange={() => onClick('탁구')}
+    />
+  </RadioTileGroup>
 );
 
 export default Sport;
