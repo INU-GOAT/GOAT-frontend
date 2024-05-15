e904044a37fc0b1f5fe718ac5890ea907ee4c459
bs8841 <whqortjs@gmail.com>
2024-05-16 01:51:35 +0900

    5-16.2

--- src/components/Sport.jsx
+++ src/components/Sport.jsx
@@ -1,42 +1,65 @@
 import React from 'react';
 import { RadioGroup } from "@mui/material";
 import { SoccerCard, BasketBallCard, BadmintonCard, TableTennisCard } from './../components/SportsCards';
-import './Sport.css';
 
 const Sport = ({ sport, setSport, preferSport, setPreferSport, disabled }) => {
-  const handleCardClick = (value) => {
-    setPreferSport(value);
-    setSport(value);
+  const handleRadioChange = (event) => {
+    setPreferSport(event.target.value);
+    setSport(event.target.value);
   };
 
   return (
     <div className="sport-selection-group">
-      <div className="sport-radio-container">
-        <RadioGroup
-          name="preferSport1"
-          value={preferSport}
-          className="sport-radio-group"
-        >
-          <div className="card-wrapper">
-            <SoccerCard sportState={{ sport, setSport: () => handleCardClick('soccer') }} disabled={disabled} />
-          </div>
-          <div className="card-wrapper">
-            <BasketBallCard sportState={{ sport, setSport: () => handleCardClick('basketBall') }} disabled={disabled} />
-          </div>
-        </RadioGroup>
-        <RadioGroup
-          name="preferSport2"
-          value={preferSport}
-          className="sport-radio-group"
-        >
-          <div className="card-wrapper">
-            <BadmintonCard sportState={{ sport, setSport: () => handleCardClick('badminton') }} disabled={disabled} />
-          </div>
-          <div className="card-wrapper">
-            <TableTennisCard sportState={{ sport, setSport: () => handleCardClick('tableTennis') }} disabled={disabled} />
-          </div>
-        </RadioGroup>
-      </div>
+      <RadioGroup
+        name="preferSport"
+        value={preferSport}
+        onChange={handleRadioChange}
+        sx={{
+          flexDirection: "row",
+          mt: 2,
+          justifyContent: "space-around",
+          alignContent: "space-around",
+          flexWrap: "wrap", // 한 줄로 다 표시되지 않을 때, 다음 줄로 넘어가도록 설정
+        }}
+      >
+        <SoccerCard
+          sportState={{ sport, setSport }}
+          label="축구"
+          disabled={disabled}
+          value="soccer"
+        />
+        <BasketBallCard
+          sportState={{ sport, setSport }}
+          label="농구"
+          disabled={disabled}
+          value="basketball"
+        />
+      </RadioGroup>
+      <RadioGroup
+        name="preferSport"
+        value={preferSport}
+        onChange={handleRadioChange}
+        sx={{
+          flexDirection: "row",
+          mt: 2,
+          justifyContent: "space-around",
+          alignContent: "space-around",
+          flexWrap: "wrap", // 한 줄로 다 표시되지 않을 때, 다음 줄로 넘어가도록 설정
+        }}
+      >
+        <BadmintonCard
+          sportState={{ sport, setSport }}
+          label="배드민턴"
+          disabled={disabled}
+          value="badminton"
+        />
+        <TableTennisCard
+          sportState={{ sport, setSport }}
+          label="탁구"
+          disabled={disabled}
+          value="tabletennis"
+        />
+      </RadioGroup>
     </div>
   );
 };
