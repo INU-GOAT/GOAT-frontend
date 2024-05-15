05bb711c535f055e0bc704eefea796aac970dfba
bs8841 <whqortjs@gmail.com>
2024-05-16 00:55:12 +0900

    5-16.1

--- src/components/Sport.jsx
+++ src/components/Sport.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RadioGroup } from "@mui/material";
 import { SoccerCard, BasketBallCard, BadmintonCard, TableTennisCard } from './../components/SportsCards';
-
+import './Sport.css';
 
 const Sport = ({ sport, setSport, preferSport, setPreferSport, disabled }) => {
   const handleCardClick = (value) => {
@@ -11,22 +11,32 @@ const Sport = ({ sport, setSport, preferSport, setPreferSport, disabled }) => {
 
   return (
     <div className="sport-selection-group">
-      <RadioGroup
-        name="preferSport"
-        value={preferSport}
-        sx={{
-          flexDirection: "row",
-          mt: 2,
-          justifyContent: "space-around",
-          alignContent: "space-around",
-          flexWrap: "wrap",
-        }}
-      >
-        <SoccerCard sportState={{ sport, setSport: () => handleCardClick('soccer') }} disabled={disabled} />
-        <BasketBallCard sportState={{ sport, setSport: () => handleCardClick('basketBall') }} disabled={disabled} />
-        <BadmintonCard sportState={{ sport, setSport: () => handleCardClick('badminton') }} disabled={disabled} />
-        <TableTennisCard sportState={{ sport, setSport: () => handleCardClick('tableTennis') }} disabled={disabled} />
-      </RadioGroup>
+      <div className="sport-radio-container">
+        <RadioGroup
+          name="preferSport1"
+          value={preferSport}
+          className="sport-radio-group"
+        >
+          <div className="card-wrapper">
+            <SoccerCard sportState={{ sport, setSport: () => handleCardClick('soccer') }} disabled={disabled} />
+          </div>
+          <div className="card-wrapper">
+            <BasketBallCard sportState={{ sport, setSport: () => handleCardClick('basketBall') }} disabled={disabled} />
+          </div>
+        </RadioGroup>
+        <RadioGroup
+          name="preferSport2"
+          value={preferSport}
+          className="sport-radio-group"
+        >
+          <div className="card-wrapper">
+            <BadmintonCard sportState={{ sport, setSport: () => handleCardClick('badminton') }} disabled={disabled} />
+          </div>
+          <div className="card-wrapper">
+            <TableTennisCard sportState={{ sport, setSport: () => handleCardClick('tableTennis') }} disabled={disabled} />
+          </div>
+        </RadioGroup>
+      </div>
     </div>
   );
 };
