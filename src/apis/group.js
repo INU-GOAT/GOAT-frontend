06c10623549ac6f5638161c84f9eaa98e8dfe693
bs8841 <whqortjs@gmail.com>
2024-05-22 22:50:19 +0900

    5-22.7

--- src/apis/group.js
+++ src/apis/group.js
@@ -8,7 +8,7 @@ groupAxios.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
+      config.headers.Authorization = `${accessToken}`;
     }
     return config;
   },
