1edf3b396ff01380c7009031b8bc991d5e79517f
bs8841 <whqortjs@gmail.com>
2024-05-29 22:09:57 +0900

    proto.v1

--- src/apis/notification.js
+++ src/apis/notification.js
@@ -35,19 +35,32 @@ export const connectNotificationSSE = () => {
     return null;
   }
   
-  const sse = new EventSource(`http://15.165.113.9:8080/api/notification/connect?access_token=${accessToken}`);
-  console.log('SSE 알림 연결 시도:', sse.url);
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://15.165.113.9:8080/api/notification/connect`, true);
+  xhr.setRequestHeader('Auth', accessToken);
+  xhr.setRequestHeader('Accept', 'text/event-stream');
 
-  sse.onopen = () => {
-    console.log('SSE 알림 연결 성공');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 2 && xhr.status === 200) {
+      const sse = new EventSource(`http://15.165.113.9:8080/api/notification/connect?access_token=${accessToken}`);
+      console.log('SSE 알림 연결 시도:', sse.url);
+
+      sse.onopen = () => {
+        console.log('SSE 알림 연결 성공');
+      };
+
+      sse.onerror = (error) => {
+        console.error('SSE 연결 오류:', error);
+        sse.close();
+      };
+
+      return sse;
+    } else if (xhr.readyState === 4 && xhr.status !== 200) {
+      console.error('SSE 연결 오류: 서버가 연결을 거부했습니다.');
+    }
   };
 
-  sse.onerror = (error) => {
-    console.error('SSE 연결 오류:', error);
-    sse.close();
-  };
-
-  return sse;
+  xhr.send();
 };
 
 export const deleteNotification = async (notificationId) => {
