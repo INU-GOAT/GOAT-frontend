0e1e98bad292cc87451213fe607cd01600fcfbf2
bs8841 <49220565+bs8841@users.noreply.github.com>
2024-05-31 09:49:29 +0900

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -28,7 +28,7 @@ const Notification = ({ onDelete, showNotifications, setShowNotifications }) => 
       const newNotification = JSON.parse(event.data);
       console.log("New Notification Received: ", newNotification);
       setLocalNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
-      setShowNotifications(true);
+      setShowNotifications(true); // 알림창을 자동으로 팝업
 
       if (Notification.permission === "granted") {
         new Notification(newNotification.content);
